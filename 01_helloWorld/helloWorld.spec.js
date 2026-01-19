346fa5545449d9ff9f2a3bfbf2fd7db64b6ef294
sachi <csamie95@gmail.com>
2026-01-19 11:43:56 +0900

    npm test was passed

--- 01_helloWorld/helloWorld.spec.js
+++ 01_helloWorld/helloWorld.spec.js
@@ -1,7 +1,10 @@
 const helloWorld = require('./helloWorld');
 
-describe('Hello World', function() {
-  test('says "Hello, World!"', function() {
+describe('Hello World', function () {
+  test('says "Hello, World!"', function () {
     expect(helloWorld()).toEqual('Hello, World!');
   });
 });
+
+
+console.log(test)
